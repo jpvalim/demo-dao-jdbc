aa9e0b74121600c419f1944a27326ac496a72b58
Jefferson Valim <jpvalim@hotmail.com>
2021-06-17 16:35:25 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,15 @@ public class Program {
 				
 			
 		}
+		
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+				
+			
+		}
 	}
 
 }
